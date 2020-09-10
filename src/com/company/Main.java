4eefada32f5d7c8b18076121258509b0b821423c
Leszek Ucinski <leszek.ucinski@student.kyh.se>
2020-09-10 13:53:42 +0200

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@ public class Main {
             System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1, -11, 24, 2));
 
             System.out.println(" ");
+
+            uyhghgvkhjb
     }
 
     public static String meanCalc(int a, int b, int c, int d){
